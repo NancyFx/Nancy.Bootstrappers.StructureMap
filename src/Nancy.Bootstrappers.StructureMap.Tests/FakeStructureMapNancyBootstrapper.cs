a8f9c4147fd0122443dbaa3148c563a215dbf0ac
Andreas Hakansson <andreas@selfinflicted.org>
2011-07-24 09:57:07 +0200

    Updated Rake, nuspec and dependencies

---
+++ src/Nancy.Bootstrappers.StructureMap.Tests/FakeStructureMapNancyBootstrapper.cs
@@ -0,0 +1,38 @@
+namespace Nancy.BootStrappers.StructureMap.Tests
+{
+    using Nancy.Bootstrappers.StructureMap;
+    using Nancy.Tests.Fakes;
+
+    using global::StructureMap;
+
+    public class FakeStructureMapNancyBootstrapper : StructureMapNancyBootstrapper
+    {
+        public bool ApplicationContainerConfigured { get; set; }
+
+        public IContainer Container
+        {
+            get { return ApplicationContainer; }
+        }
+
+        public bool RequestContainerConfigured { get; set; }
+
+        protected override void ConfigureApplicationContainer(IContainer existingContainer)
+        {
+            this.ApplicationContainerConfigured = true;
+            base.ConfigureApplicationContainer(existingContainer);
+        }
+
+        protected override void ConfigureRequestContainer(IContainer container)
+        {
+            base.ConfigureRequestContainer(container);
+
+            RequestContainerConfigured = true;
+
+            container.Configure(registry =>
+            {
+                registry.For<IFoo>().Singleton().Use<Foo>();
+                registry.For<IDependency>().Singleton().Use<Dependency>();
+            });
+        }
+    }
+}
---
+++ src/SharedAssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("Nancy.Bootstrappers.StructureMap")]
+[assembly: AssemblyDescription("An StructureMap Bootstrapper for the Nancy web framework")]
+[assembly: AssemblyCompany("Nancy")]
+[assembly: AssemblyProduct("Nancy.Bootstrappers.StructureMap")]
+[assembly: AssemblyCopyright("Copyright (C) Andreas Hakansson, Steven Robbins and contributors")]
+[assembly: AssemblyVersion("0.6.0")]
+
