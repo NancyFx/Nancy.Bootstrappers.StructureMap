0c0dc6930f3e7d9ae1e5a350fa65e1c0c53425dc
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2014-06-16 10:58:45 +0200

    Remove the reference to ObjectFactory in RegisterBootstrapperTypes()

--- src/Nancy.Bootstrappers.StructureMap/StructureMapNancyBootstrapper.cs
+++ src/Nancy.Bootstrappers.StructureMap/StructureMapNancyBootstrapper.cs
@@ -102,10 +102,7 @@
                 // constructor, just the greediest
                 registry.For<IFileSystemReader>().Singleton().Use<DefaultFileSystemReader>();
 
-                // DefaultRouteCacheProvider doesn't have a parameterless constructor.
-                // It has a Func<IRouteCache> parameter, which StructureMap doesn't know how to handle
-                var routeCacheFactory = new Func<IRouteCache>(ObjectFactory.GetInstance<IRouteCache>);
-                registry.For<Func<IRouteCache>>().Use(routeCacheFactory);
+                registry.For<IRouteCacheProvider>().Use<DefaultRouteCacheProvider>();
             });
         }
 
